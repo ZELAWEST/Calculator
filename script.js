9429d45989a2ab5e4b5a104183ee0c6c0516f788
zelawest <169395273+ZELAWEST@users.noreply.github.com>
2024-07-11 20:18:07 +0200

    Add functions addition,subtraction,division

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ let digitButtons = document.querySelectorAll('.digit')
 let equal = document.querySelector('.equals')
 let clear = document.querySelector('#clear')
 let dot = document.querySelector('.dot')
+
+function additionOperation(a, b) {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    return a + b
+}
+function subtractionOperation(a, b) {
+    return a - b
+}
+function divisionOperation(a, b) {
+    return a / b
+}
+function multiplicationOperation(a, b) {
+    return a * b
+}
